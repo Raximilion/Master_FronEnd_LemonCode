7fe98ad66033676f80add587004b157062b5441e
raximilion <raximilion@gmail.com>
2021-10-18 12:52:29 +0200

    public menu

--- module4Angular/src/app/app-routing.module.ts
+++ module4Angular/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/about/about/about.component';
+import { HomeComponent } from './components/home/home/home.component';
+import { LoginComponent } from './components/login/login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'about', component:AboutComponent },
+  {path:'home', component:HomeComponent },
+  {path:'login', component:LoginComponent },
+  {path:'**',  pathMatch: 'full', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- module4Angular/src/app/app.module.ts
+++ module4Angular/src/app/app.module.ts
@@ -4,14 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login/login.component';
 import { AboutComponent } from './components/about/about/about.component';
-import { LayoutComponent } from './components/layout/layout.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { GaleryComponent } from './components/galery/galery.component';
-import { CrudComponent } from './components/crud/crud.component';
-import { ProfileComponent } from './components/profile/profile.component';
+import { HomeComponent } from './components/home/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
@@ -19,11 +15,7 @@ import { ProfileComponent } from './components/profile/profile.component';
     HomeComponent,
     LoginComponent,
     AboutComponent,
-    LayoutComponent,
-    DashboardComponent,
-    GaleryComponent,
-    CrudComponent,
-    ProfileComponent
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
